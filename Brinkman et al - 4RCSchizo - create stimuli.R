f16d1bc6637bd4dfb0f346ed7e2eeffb05f22e4a
Loek Brinkman <loekbrinkman@gmail.com>
2018-11-29 12:25:51 +0100

    Update Brinkman et al - 4RCSchizo - create stimuli.R

--- Brinkman et al - 4RCSchizo - create stimuli.R
+++ Brinkman et al - 4RCSchizo - create stimuli.R
@@ -2,5 +2,5 @@ library(rcicr)
 base_face_files <- list('face'='MNES.jpg')
 generateStimuli2IFC(base_face_files, n_trials = 900)
 
-#for the RC schizo study, we only use the images with the extentio '*_ori.jpg'.
+#for the RC schizo study, we only use the images with the extentio '*_ori.png'.
 #see manuscripts for details of the experiment.
